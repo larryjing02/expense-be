7df08a6235bf22bee6f046d889c3d3076a573153
Larry Jing <larryjing02@gmail.com>
2023-06-27 00:05:44 -0700

    Push template files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add MongoDB services to the DI system
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var settings = builder.Configuration.GetSection("MongoDb");
+    var client = new MongoClient(settings.GetSection("ConnectionString").Value);
+    return client;
+});
+
+builder.Services.AddScoped(serviceProvider =>
+{
+    var client = serviceProvider.GetRequiredService<IMongoClient>();
+    var settings = builder.Configuration.GetSection("MongoDb");
+    var database = client.GetDatabase(settings.GetSection("DatabaseName").Value);
+    return database;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
